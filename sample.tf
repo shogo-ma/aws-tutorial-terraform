070ea295edeae5499605105f44f4a39a66896851
shogo-ma <Choroma194@gmail.com>
2018-05-05 00:48:16 +0900

    private subnetを追加

--- sample.tf
+++ sample.tf
@@ -18,6 +18,15 @@ resource "aws_subnet" "public_subnet" {
     }
 }
 
+resource "aws_subnet" "private_subnet" {
+    vpc_id = "${aws_vpc.vpc_region.id}"
+    cidr_block = "10.0.2.0/24"
+
+    tags {
+        Name = "プライベートサブネット"
+    }
+}
+
 resource "aws_internet_gateway" "gateway" {
     vpc_id = "${aws_vpc.vpc_region.id}"
 }
