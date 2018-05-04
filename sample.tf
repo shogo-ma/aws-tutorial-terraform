ff24d5cb67fd09743e1a34026738bcbdda5ce128
shogo-ma <Choroma194@gmail.com>
2018-05-04 18:13:38 +0900

    ルートテーブルの設定追加

--- sample.tf
+++ sample.tf
@@ -19,3 +19,20 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_internet_gateway" "gateway" {
     vpc_id = "${aws_vpc.vpc_region.id}"
 }
+
+resource "aws_route_table" "rt" {
+    vpc_id = "${aws_vpc.vpc_region.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gateway.id}"
+    }
+
+    tags {
+        Name = "パブリックルートテーブル"
+    }
+}
+
+resource "aws_route_table_association" "public_r" {
+    subnet_id = "${aws_subnet.public_subnet.id}"
+    route_table_id = "${aws_route_table.rt.id}"
+}
