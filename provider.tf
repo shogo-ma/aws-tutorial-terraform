be506481edfc0fbb7fd3b8ee936d29374b9f3dbf
shogo-ma <Choroma194@gmail.com>
2018-05-07 21:47:50 +0900

    変数をvariables.tfに移動

--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_access_key" {}
-variable "ssh_key_file" {}
-
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_access_key}"
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "aws_access_key" {}
+variable "aws_secret_access_key" {}
+variable "ssh_key_file" {}
